013176160a30120f88e737e3913e4829777c5585
Allison Logan <allisonlogan@ufl.edu>
2017-08-15 10:44:32 -0400

    adds countdown clock

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
         social_tools:false,
         overlay_gallery: true,
         markup: '<div class="pp_pic_holder"> \
-						<div class="ppt">&nbsp;</div> \
+						<div class="ppt"> </div> \
 						<div class="pp_top"> \
 							<div class="pp_left"></div> \
 							<div class="pp_middle"></div> \
@@ -71,3 +71,37 @@
 	});
 });
 
+
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 4, 2017 12:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in each element with id
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+         
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("days").innerHTML = 0;
+    	document.getElementById("hours").innerHTML = 0;
+    	document.getElementById("minutes").innerHTML = 0;
+   		document.getElementById("seconds").innerHTML = 0;
+    }
+}, 1000);
